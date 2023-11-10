30957382b4156377d800b3ef5c60452caf7e7a73
bakryabdalsalam <bakryabdalsalam6@gmail.com>
2023-11-10 18:24:37 +0200

    fix code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('An error occurred:', error);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-
-  filmData.characters.forEach((characterUrl, index) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('An error occurred:', error);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters; 
+  exact0rder(actors, 0);
 });
+const exact0rder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, response, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exact0rder(actors, x + 1);
+  });
+};
